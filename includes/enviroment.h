f53bbd0fab00adaa2e98b315f711334f403b5670
WhizdomConjurer <>
2025-06-01 16:57:40 +0200

    building the enviroment and open shells

--- includes/enviroment.h
+++ includes/enviroment.h
@@ -6,7 +6,7 @@
 /*   By: reriebsc <reriebsc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 10:49:15 by reriebsc          #+#    #+#             */
-/*   Updated: 2025/05/29 13:06:36 by reriebsc         ###   ########.fr       */
+/*   Updated: 2025/06/01 14:27:06 by reriebsc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define ENVIROMENT_H
 
 #include <stdlib.h>
+#include "../libft_extend/libft.h"
 
 char    **ms_env_gen(char **envp);
+void	ms_free_envc(char **envc);
 
 #endif
--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: reriebsc <reriebsc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:28:53 by reriebsc          #+#    #+#             */
-/*   Updated: 2025/05/29 11:55:29 by reriebsc         ###   ########.fr       */
+/*   Updated: 2025/06/01 15:51:12 by reriebsc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,29 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdlib.h>
+#include "../libft_extend/libft.h"
+#include <unistd.h>
 
 
+//*************************************************************/
+// Structs 
+//*************************************************************/
+
+typedef struct s_dict_env
+{
+	char				*key;
+	char				*value;
+}						t_dict_env;
+
+typedef struct s_minishell
+{
+    t_list          *env;
+}                   t_minishell;
+
+
+//*************************************************************/
+// Enviroment
+//*************************************************************/
 
 #endif
