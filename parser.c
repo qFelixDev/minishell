ecbb2b25489f7b18cb4d3bae137247ab4ab542c7
qFelix <>
2025-06-03 12:33:52 +0200

    Finish tokenizer

--- parser.c
+++ parser.c
@@ -6,126 +6,159 @@
 /*   By: ghodges <ghodges@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 10:31:52 by ghodges           #+#    #+#             */
-/*   Updated: 2025/06/03 10:57:02 by ghodges          ###   ########.fr       */
+/*   Updated: 2025/06/03 12:30:42 by ghodges          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*enum {
-	MS_OR,
-	MS_AND,
-	MS_PIPE
+#include <stdbool.h>
+#include "libft_extend/libft.h"
+
+typedef struct s_ms_token {
+	struct s_ms_token	*next;
+	int					index;
+	char				*string;
+}	t_ms_token;
+
+bool	ms_isspace(char character)
+{
+	return ((character >= 9 && character <= 13) || character == 32);
 }
 
-typedef struct s_ms_command {
-	char	**argv; // [0] = "echo", [1] = "bananenkuchen"
-	char	*input; // NULL
-	char	*output; // NULL
-	char	*delimited; // NULL
-	char	*append; // NULL
-}	t_ms_command;
-
-typedef struct s_ms_sequence {
-	uint8_t	behavior;
-	uint8_t	*is_sequence;
-	void	**objects;
-}	t_ms_sequence;
-
-typedef struct s_ms_string_sequence {
-	uint8_t	*is_sequence;
-	void	**objects;
-}	t_ms_string_sequence;
-
-// ls && ls || ls && ls
-// (ls && (ls || ls) && ls)
-
-char	*skip_brackets(char *string)
+char	*create_token_string(char delimiter, char *string)
 {
-	string++;
-	while (*string != ')' && *string != '\0')
-	{
-		if (*string == '(')
-			string = skip_brackets(string);
-		else if (*string == '"')
-			string = skip_quotes(string, '"');
-		else if (*string == '\'')
-			string = skip_quotes(string, '\'');
-		string++;
-	}
-	string += (*string == ')');
-	return (string);
-}
+	char	*token_string;
+	size_t	length;
 
-char	*skip_quotes(char *string, char quote)
-{
-	string++;
-	while (*string != quote && *string != '\0')
-		string++;
-	if (*string == '\0')
-		return (NULL);
-	return (string + 1);
-}
-
-char	*get_next_seperator(char *string, char *seperator)
-{
-	while (*string != ')')
-	{
-		if (strncmp(string, seperator, strlen(seperator)) == 0)
-			return (string);
-		if (*string == '(')
-			string = skip_brackets(string);
-		else if (*string == '"')
-			string = skip_quotes(string, '"');
-		else if (*string == '\'')
-			string = skip_quotes(string, '\'');
-		string++;
-	}
-	return (NULL);
-}
-
-char	*create_token(t_ms_token *token, char *string)
-{
-	char const *const	tokens[9] =
-		{"&&", "||", "|", "<<", ">>", "<", ">", "'", "\"", ""};
-	size_t				length;
-
-	token -> index = -1;
-	while (++token -> index < 9)
-	{
-		if (strncmp(string, tokens[token -> index],
-			strlen(tokens[token -> index])) == 0)
-			break ;
-	}
-	string += strlen(tokens[token -> index]);
-	if (token -> index < 7)
-		return (string);
 	length = 0;
-	while (string[length] )
-}*/
-
-char	*create_token(t_ms_token *token, char *string)
-{
-	char const *const	tokens[12]
-		= {"&&", "||", "|", "<<", ">>", "<", ">", "(", ")", "'", "\"", ""};
-	size_t				length;
-
-	token -> index = -1;
-	while (++token -> index < 11)
-		if (ft_strncmp(string, tokens[token -> index],
-				ft_strlen(tokens[token -> index])) == 0)
-			break ;
-	string += ft_strlen(tokens[token -> index]);
-	if (token -> index < 9)
-		return (string);
-	length = 0;
-	if (token -> index < 11)
-		while (string[length] != *tokens[token->index] && string[length] != 0)
+	if (delimiter == '\0')
+		while (ft_strchr("|<>()'\"", string[length]) == NULL
+			&& ft_strncmp(string + length, "&&", 2) != 0)
 			length++;
 	else
-		while (ft_strchr("|<>()'\"", string[length]) == NULL
-			&& ft_strncmp(string[length], "&&", 2) != 0)
+	{
+		while (string[length] != delimiter && string[length] != '\0')
 			length++;
-	token -> string = malloc(length + 1, 1);
-	if (token -> string 
-	ft_strlcpy(token -> string, )
-	return (string + length + ft_strlen(tokens[token -> index]));
+		if (string[length] == '\0')
+			return (NULL);
+	}
+	token_string = malloc(length + 1);
+	if (token_string == NULL)
+		return (NULL);
+	ft_strlcpy(token_string, string, length + 1);
+	return (token_string);
+}
+
+#include<stdio.h>
+
+t_ms_token	*create_token(char **string_pt)
+{
+	char const *const	token_strings[12]
+		= {"&&", "||", "|", "<<", ">>", "<", ">", "(", ")", "'", "\"", ""};
+	t_ms_token *const	token = malloc(sizeof(t_ms_token));
+	size_t				length;
+
+	if (token == NULL)
+		return (NULL);
+	token -> next = NULL;
+	token -> index = -1;
+	while (++token -> index < 11) {
+		if (ft_strncmp(*string_pt, token_strings[token -> index],
+				ft_strlen(token_strings[token -> index])) == 0)
+			break ;
+	}
+	*string_pt += ft_strlen(token_strings[token -> index]);
+	if (token -> index < 9)
+		return (token);
+	token -> string = create_token_string(*token_strings[token -> index], *string_pt);
+	*string_pt += ft_strlen(token -> string);
+	*string_pt += ft_strlen(token_strings[token -> index]);
+	if (token -> string == NULL)
+		return (free(token), NULL);
+	return (token);
+}
+
+void	free_tokens(t_ms_token *token)
+{
+	t_ms_token	*temp;
+
+	while (token != NULL)
+	{
+		temp = token;
+		token = token -> next;
+		if (token -> index >= 9)
+			free(temp -> string);
+		free(temp);
+	}
+}
+
+t_ms_token	*ms_tokenize(char *string)
+{
+	t_ms_token	*first;
+	t_ms_token	*token;
+
+	while (ms_isspace(*string))
+		string++;
+	first = create_token(&string);
+	if (first == NULL)
+		return (free_tokens(first), NULL);
+	token = first;
+	while (*string != '\0')
+	{
+		token -> next = create_token(&string);
+		token = token -> next;
+		if (token == NULL)
+			return (free_tokens(first), NULL);
+		while (ms_isspace(*string))
+			string++;
+	}
+	return (first);
+}
+
+int main(int argc, char** argv) {
+	if(argc < 2)
+		return 0;
+	t_ms_token* first = ms_tokenize(argv[1]);
+	t_ms_token* token = first;
+	while(token != NULL) {
+		switch(token -> index) {
+			case 0:
+				puts("MS_AND");
+				break;
+			case 1:
+				puts("MS_OR");
+				break;
+			case 2:
+				puts("MS_PIPE");
+				break;
+			case 3:
+				puts("MS_DELIMITED");
+				break;
+			case 4:
+				puts("MS_APPEND");
+				break;
+			case 5:
+				puts("MS_INPUT");
+				break;
+			case 6:
+				puts("MS_OUTPUT");
+				break;
+			case 7:
+				puts("MS_OPEN");
+				break;
+			case 8:
+				puts("MS_CLOSE");
+				break;
+			case 9:
+				puts("MS_TIGHT_STRING");
+				break;
+			case 10:
+				puts("MS_LOOSE_STRING");
+				break;
+			case 11:
+				puts("MS_OPEN_STRING");
+				break;
+		}
+		token = token -> next;
+	}
+	//free_tokens(first);
 }
