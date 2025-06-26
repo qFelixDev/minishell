3dae60a5ee6477b3d94f50e73a990ed3dd1cf815
qFelix <>
2025-06-26 18:18:28 +0200

    Substantial parser progress

--- parser/ms_get_command.c
+++ parser/ms_get_command.c
@@ -6,7 +6,7 @@
 /*   By: ghodges <ghodges@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 14:15:33 by ghodges           #+#    #+#             */
-/*   Updated: 2025/06/25 11:28:57 by ghodges          ###   ########.fr       */
+/*   Updated: 2025/06/26 17:18:36 by ghodges          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,91 +85,147 @@ bool	ruin_delimiters(t_ms_token *token)
 	return (true);
 }
 
-void	get_pattern_quantities(
-	t_ms_token *first, size_t *pattern_length_pt, size_t *asterisk_count_pt)
+size_t	apply_working_directory(t_ms_token *token, t_ms_pattern *pattern)
 {
-	char	*string;
+	char	*working_directory;
 
-	*pattern_length_pt = 0;
-	*asterisk_count_pt = 0;
-	while (token != NULL)
+	if (!(token -> index == MS_TOKEN_STRING && token -> content[0] == '/'))
 	{
+		working_directory = getcwd(NULL, 0);
+		if (pattern != NULL)
+			ft_strlcpy(pattern -> string, working_directory,
+				ft_strlen(working_directory) + 1);
+		return (ft_strlen(working_directory));
+	}
+	return (0);
+}
+
+size_t	ms_populate_pattern(t_ms_token *token, t_ms_pattern *pattern)
+{
+	size_t		length;
+
+	length = apply_working_directory(token, pattern);
+	while (token != NULL && token -> index >= MS_TOKEN_WILDCARD)
+	{
+		if (pattern != NULL && token -> index == MS_TOKEN_STRING)
+			ft_strlcpy(pattern -> string + length, token -> content,
+				ft_strlen(token -> content) + 1);
+		if (pattern != NULL && token -> index == MS_TOKEN_WILDCARD)
+			pattern -> is_wildcard[length / 8] |= (1u << (length % 8));
 		if (token -> index == MS_TOKEN_STRING)
-		{
-			string = token -> content;
-			while (*string != '\0')
-			{
-				(*pattern_length_pt)++;
-				*asterisk_count_pt += (*string == '*');
-				string++;
-			}
-		}
-		*asterisk_count_pt += (token -> index == MS_TOKEN_WILDCARD
-			&& (token -> next == NULL
-				|| token -> next -> index == MS_TOKEN_STRING
-				|| !token -> concatenate_content));
+			length += ft_strlen(token -> content);
+		length += (token -> index == MS_TOKEN_WILDCARD);
 		if (!token -> concatenate_content)
 			break ;
 		token = token -> next;
 	}
+	return (length);
 }
 
-size_t	add_match(t_ms_pattern *pattern)
+size_t	ms_populate_pattern(t_ms_token *token, char *pattern)
 {
-	char *const	new_path = ft_strjoin()
-	struct stat	status;
+	size_t	length;
+
+	length = 0;
+	while (token != NULL && token -> index >= MS_TOKEN_WILDCARD)
+	{
+		if (pattern != NULL)
+		{
+			if (token -> index == MS_TOKEN_STRING)
+				ft_strlcpy(pattern + length, token -> content,
+					ft_strlen(token -> content));
+			else
+				pattern[length] = '\1';
+		}
+		if (token -> index == MS_TOKEN_STRING)
+			length += ft_strlen(token -> content);
+		length += (token -> index == MS_TOKEN_WILDCARD);
+		if (!token -> concatenate_content)
+			break ;
+		token = token -> next;
+	}
+	return (length);
 }
 
-size_t	ms_enumerate_matches(t_ms_pattern *pattern, char *path, char **output)
+bool	matches_pattern(char *pattern, char *string)
+{
+	size_t	length;
+
+	word_terminator = ft_strchr(pattern, '\1');
+	while (word_terminator != NULL);
+	{
+		while (ft_strncmp(pattern, string, word_length) != 0
+			&& *string != '\0')
+			string++;
+		pattern += word_length;
+		string += word_length;
+		while (*pattern == '\1')
+			pattern++;
+		word_terminator = ft_strchr(pattern, '\1');
+	}
+}
+
+// Pass "/" to this function if path is absolute, "" if relative and the value of $HOME if preceded by a noodle
+
+size_t	ms_enumerate_matches(char *pattern, char *path, char **matches)
 {
 	DIR *const		directory = opendir(path);
 	struct dirent	*entry;
+	struct stat		status;
 	size_t			match_count;
 	size_t			result;
+	char			*new_path;
 
 	if (directory == NULL)
 		return (SIZE_MAX);
-	match_count = 0;
 	entry = readdir(directory);
 	while (entry != NULL)
 	{
 		if (matches_pattern(pattern, entry -> d_name))
 		{
-			result = add_match();
-			if (result == SIZE_MAX)
-				return (SIZE_MAX);
-			match_count += result;
+			new_path = ft_strjoin(path, entry -> d_name);
+			if (new_path == NULL || lstat(new_path, &status) == -1)
+				return (closedir(directory), free(path), SIZE_MAX);
+			if (ft_strchr(pattern, '/') == NULL)
+			{
+				if (matches != NULL)
+					matches[match_count] = new_path;
+				match_count++;
+			}
+			else if (S_ISDIR(status.st_mode))
+			{
+				result = ms_populate_matches(next_pattern(pattern), matches + match_count);
+				if (result == SIZE_MAX)
+					return (closedir(directory), free(path), SIZE_MAX);
+				match_count += result;
+			}
 		}
 		entry = readdir(directory);
 	}
-	free(path);
 	closedir(directory);
+	free(path);
 	return (match_count);
 }
 
-size_t	ms_enumerate_matches(t_ms_pattern *pattern, char *path, char **output)
+size_t	ms_enumerate_matches(t_ms_token *token, char **matches)
 {
-	DIR *const		directory = opendir(path);
-	struct dirent	*entry;
-	size_t			match_count;
+	t_ms_pattern	pattern;
 	size_t			result;
 
-	if (directory == NULL)
+	result = ms_populate_pattern(token, NULL);
+	if (result == SIZE_MAX)
 		return (SIZE_MAX);
-	match_count = 0;
-	entry = readdir(directory);
-	while (entry != NULL)
-	{
-		if (matches_pattern(pattern, entry -> d_name))
-		{
-			result = add_match();
-			if (result == SIZE_MAX)
-				return (SIZE_MAX);
-			match_count += result;
-		}
-		entry = readdir(directory);
-	}
-	return (match_count);
+	pattern.string = malloc(pattern_length + 1);
+	pattern.is_wildcard = malloc((pattern_length / 8) + 1);
+	if (pattern.string == NULL || pattern.is_wildcard == NULL)
+		return (free(pattern.string), free(pattern.is_wildcard), SIZE_MAX);
+	result = ms_populate_pattern(token, NULL);
+	if (result == SIZE_MAX)
+		return (SIZE_MAX);
+	result = ms_populate_matches(pattern, );
+	free(pattern.string);
+	free(pattern.is_wildcard);
+	return (result);
 }
 
 int	count_arguments(t_ms_token *token)
@@ -189,7 +245,7 @@ int	count_arguments(t_ms_token *token)
 	{
 		if (!token -> concatenate_content
 			&& token -> next -> index >= MS_TOKEN_WILDCARD)
-			if (!ms_enumerate_matches(token, NULL))
+			if (ms_enumerate_matches(token, NULL) == SIZE_MAX)
 				return (-1);
 		token = token -> next;
 	}
@@ -251,11 +307,13 @@ t_ms_command	*ms_allocate_command(t_ms_token *token)
 
 bool	check_ambiguity(t_ms_token *token)
 {
-	while (token != NULL)
+	if (token == NULL)
+		return (true);
+	while (token -> next != NULL)
 	{
 		if (token -> index >= MS_TOKEN_APPEND
 			&& token -> index <= MS_TOKEN_OUTPUT
-			&& ms_enumerate_matches(token, NULL) != 1)
+			&& ms_enumerate_matches(token -> next, NULL) != 1)
 			return (false);
 		token = token -> next;
 	}
