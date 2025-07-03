6ea412da50c9702af4ecc95c0924572a6e9911c1
qFelix <>
2025-07-03 15:10:21 +0200

    Improve norm compliance of wildcard expansion

--- parser/ms_expand_wildcards.c
+++ parser/ms_expand_wildcards.c
@@ -6,7 +6,7 @@
 /*   By: ghodges <ghodges@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 20:27:54 by ghodges           #+#    #+#             */
-/*   Updated: 2025/06/30 20:38:35 by ghodges          ###   ########.fr       */
+/*   Updated: 2025/07/03 15:06:28 by ghodges          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,78 +37,91 @@ bool	matches_pattern(char *pattern, char *string)
 	if (pattern[0] != '.' && string[0] == '.')
 		return (false);
 	length = get_word_length(pattern);
-	if (pattern[length] == '\1')
-		while (true)
-		{
-			if (ft_strncmp(pattern, string, length) != 0)
-				return (false);
-			pattern += length;
-			string += length;
-			while (*pattern == '\1')
-				pattern++;
-			length = get_word_length(pattern);
-			if (pattern[length] != '\1')
-				break ;
-			while (ft_strncmp(pattern, string, length) != 0 && *string != '\0')
-				string++;
-		}
-	while (ft_strncmp(pattern, string, length) != 0 && *string != '\0')
+	if (pattern[length] != '\1')
+		return (ft_strncmp(pattern, string, length) == 0
+			&& ft_strlen(string) == length);
+	while (true)
+	{
+		if (ft_strncmp(pattern, string, length) != 0)
+			return (false);
+		pattern += length;
+		string += length;
+		while (*pattern == '\1')
+			pattern++;
+		length = get_word_length(pattern);
+		if (pattern[length] != '\1')
+			break ;
+		while (ft_strncmp(pattern, string, length) != 0 && *string != '\0')
+			string++;
+	}
+	while ((ft_strncmp(pattern, string, length) != 0
+			|| ft_strlen(string) != length) && *string != '\0')
 		string++;
+	//printf("%zu %zu\n", ft_strlen(string), length);
 	return (ft_strncmp(pattern, string, length) == 0);
 }
 
-// Pass "/" to this function if path is absolute, "" if relative and the value of $HOME if preceded by a noodle
+//int main() {
+//	printf("%d\n", matches_pattern("_parser", "_compile_parser.sh"));
+//}
 
-char	*next_pattern(char *pattern)
-{
-	while (*pattern != '/')
+void print_pattern(char *pattern) {
+	while(*pattern != '\0') {
+		if(*pattern == '\1')
+			printf("[*]");
+		else
+			putchar(*pattern);
 		pattern++;
-	while (*pattern == '/')
-		pattern++;
-	return (pattern);
+	}
+	printf("\n");
 }
 
-size_t	enumerate_matches(char *pattern, char *path, char **matches, size_t offset)
+size_t	enumerate_matches(char *pattern, char *path, char **matches);
+
+size_t	add_match(char *pattern, char *path, char *name, char **matches)
+{
+	struct stat		status;
+	const size_t	new_length = ft_strlen(path) + ft_strlen(name) + 1;
+	char *const		new_path = ft_calloc(1, new_length + 1);
+
+	if (new_path == NULL)
+		return (0);
+	ft_strlcat(new_path, path, new_length);
+	ft_strlcat(new_path, name, new_length);
+	if (lstat(new_path, &status) == -1)
+		return (0);
+	if (ft_strchr(pattern, '/') == NULL)
+	{
+		if (matches != NULL)
+			*matches = new_path;
+	}
+	else if (S_ISDIR(status.st_mode))
+	{
+		ft_strlcat(new_path, "/", new_length + 1);
+		while (*pattern != '/')
+			pattern++;
+		while (*pattern == '/')
+			pattern++;
+		return (enumerate_matches(pattern, new_path, matches));
+	}
+	return (ft_strchr(pattern, '/') == NULL);
+}
+
+size_t	enumerate_matches(char *pattern, char *path, char **matches)
 {
 	DIR *const		directory = opendir(path);
 	struct dirent	*entry;
-	struct stat		status;
 	size_t			match_count;
-	size_t			result;
-	size_t			new_length;
-	char			*new_path;
 
 	match_count = 0;
 	if (directory == NULL)
-		return (SIZE_MAX);
+		return (0);
 	entry = readdir(directory);
 	while (entry != NULL)
 	{
 		if (matches_pattern(pattern, entry -> d_name))
-		{
-			new_length = ft_strlen(path) + ft_strlen(entry -> d_name) + 1;
-			new_path = ft_calloc(1, new_length + 1);
-			if (new_path == NULL)
-				return (closedir(directory), free(new_path), free(path), SIZE_MAX);
-			ft_strlcat(new_path, path, new_length + 1);
-			ft_strlcat(new_path, entry -> d_name, new_length + 1);
-			if (lstat(new_path, &status) == -1)
-				return (SIZE_MAX);
-			if (ft_strchr(pattern, '/') == NULL)
-			{
-				if (matches != NULL)
-					matches[offset + match_count] = new_path;
-				match_count++;
-			}
-			else if (S_ISDIR(status.st_mode))
-			{
-				ft_strlcat(new_path, "/", new_length + 1);
-				result = enumerate_matches(next_pattern(pattern), new_path, matches, match_count);
-				if (result == SIZE_MAX)
-					return (closedir(directory), free(path), SIZE_MAX);
-				match_count += result;
-			}
-		}
+			match_count += add_match(pattern, path, entry -> d_name, matches
+				+ (matches != NULL) * match_count);
 		entry = readdir(directory);
 	}
 	closedir(directory);
@@ -138,17 +151,6 @@ size_t	get_pattern(t_ms_token *token, char *pattern)
 	return (length);
 }
 
-void print_pattern(char *pattern) {
-	while(*pattern != '\0') {
-		if(*pattern == '\1')
-			printf("[*]");
-		else
-			putchar(*pattern);
-		pattern++;
-	}
-	printf("\n");
-}
-
 size_t	ms_expand_wildcards(t_ms_token *token, char **paths)
 {
 	size_t		match_count;
@@ -157,16 +159,16 @@ size_t	ms_expand_wildcards(t_ms_token *token, char **paths)
 
 	assert(token -> index >= MS_TOKEN_WILDCARD);
 	if (pattern == NULL)
-		return (SIZE_MAX);
+		return (0);
 	get_pattern(token, pattern);
 	is_absolute = (pattern[0] == '/');
 	print_pattern(pattern);
 	match_count = enumerate_matches(
-		pattern + is_absolute, ft_strdup(&"./"[is_absolute]), NULL, 0);
-	if (match_count == SIZE_MAX || paths == NULL)
+		pattern + is_absolute, ft_strdup(&"./"[is_absolute]), NULL);
+	if (paths == NULL)
 		return (match_count);
 	match_count = enumerate_matches(
-		pattern + is_absolute, ft_strdup(&"./"[is_absolute]), paths, 0);
+		pattern + is_absolute, ft_strdup(&"./"[is_absolute]), paths);
 	free(pattern);
 	return (match_count);
 }
