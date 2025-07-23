ead1807cf2058bf39b2e120358cec207c100583e
WhizdomConjurer <>
2025-07-23 15:48:55 +0200

    bug fix

--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghodges <ghodges@student.42.fr>            +#+  +:+       +#+        */
+/*   By: reriebsc <reriebsc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 10:59:39 by reriebsc          #+#    #+#             */
-/*   Updated: 2025/07/23 12:58:47 by ghodges          ###   ########.fr       */
+/*   Updated: 2025/07/23 15:48:03 by reriebsc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,35 @@ static void	handle_shell_input(char *line)
 		gc_free_ptr(line));
 }
 
+void	minishell_non_interactive_2(void)
+{
+	char	*line;
+
+	line = get_next_line(STDIN_FILENO);
+	while (line != NULL)
+	{
+		if (line[0] == '\n' || line[0] == '\0')
+		{
+			gc_free_ptr(line);
+			continue ;
+		}
+		if (line[ft_strlen(line) - 1] == '\n')
+			line[ft_strlen(line) - 1] = '\0';
+		gc_add(line);
+		handle_shell_input(line);
+		line = get_next_line(STDIN_FILENO);
+	}
+}
+
 void	non_interactive_arg(char **args, int argc)
 {
 	char	*line;
 
 	line = join_str_array(args, argc);
-	handle_shell_input(line);
+	write(STDIN_FILENO, line, ft_strlen(line));
+	write(STDIN_FILENO, "\n", 1);
+	gc_free_ptr(line);
+	minishell_non_interactive_2();
 }
 
 void	interactive(void)
