d28d870938e0d5236b15c35875c4950f0f629884
WhizdomConjurer <>
2025-07-23 17:14:31 +0200

    test

--- execution/exe_2_2.c
+++ execution/exe_2_2.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   exe_2_2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghodges <ghodges@student.42.fr>            +#+  +:+       +#+        */
+/*   By: reriebsc <reriebsc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 11:20:00 by reriebsc          #+#    #+#             */
-/*   Updated: 2025/07/23 16:44:05 by ghodges          ###   ########.fr       */
+/*   Updated: 2025/07/23 17:03:15 by reriebsc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include <sys/wait.h>
 
-void	restore_std_fds(int stdin_backup, int stdout_backup)
+static void	restore_std_fds(int stdin_backup, int stdout_backup)
 {
 	dup2(stdin_backup, STDIN_FILENO);
 	dup2(stdout_backup, STDOUT_FILENO);
@@ -21,7 +21,7 @@ void	restore_std_fds(int stdin_backup, int stdout_backup)
 	close(stdout_backup);
 }
 
-int	try_execute_builtin(t_ms_command *command, char **build_ins,
+static int	try_execute_builtin(t_ms_command *command, char **build_ins,
 				int stdin_backup, int stdout_backup)
 {
 	int	i;
@@ -40,6 +40,23 @@ int	try_execute_builtin(t_ms_command *command, char **build_ins,
 		++i;
 	}
 	return (-1);
+}
+
+static bool	apply_redirects(char **redirects, int type)
+{
+	size_t	last_index;
+	int		descriptor;
+	int		target_fd;
+
+	last_index = 0;
+	if (redirects == NULL || redirects[0] == NULL)
+		return (true);
+	if (!handle_intermediate_redirects(redirects, &last_index))
+		return (false);
+	target_fd = get_target_fd(type);
+	if (!open_redirect_fd(redirects[last_index], type, &descriptor))
+		return (false);
+	return (dup_and_close(descriptor, target_fd));
 }
 
 int	exe_manager(t_ms_command *command, int delim_descriptor)
@@ -67,21 +84,4 @@ int	exe_manager(t_ms_command *command, int delim_descriptor)
 		return (result);
 	restore_std_fds(stdin_backup, stdout_backup);
 	return (ms_execution_command(command));
-}
-
-bool	apply_redirects(char **redirects, int type)
-{
-	size_t	last_index;
-	int		descriptor;
-	int		target_fd;
-
-	last_index = 0;
-	if (redirects == NULL || redirects[0] == NULL)
-		return (true);
-	if (!handle_intermediate_redirects(redirects, &last_index))
-		return (false);
-	target_fd = get_target_fd(type);
-	if (!open_redirect_fd(redirects[last_index], type, &descriptor))
-		return (false);
-	return (dup_and_close(descriptor, target_fd));
 }
