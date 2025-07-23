98cab3a8c504fc177810073ff24b2ef5e0db8d72
WhizdomConjurer <>
2025-07-23 18:12:14 +0200

    hhhh

--- parser/ms_expand_wildcards.c
+++ parser/ms_expand_wildcards.c
@@ -6,7 +6,7 @@
 /*   By: reriebsc <reriebsc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 17:26:12 by ghodges           #+#    #+#             */
-/*   Updated: 2025/07/23 17:39:30 by reriebsc         ###   ########.fr       */
+/*   Updated: 2025/07/23 18:11:43 by reriebsc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,30 +128,4 @@ size_t	get_pattern(t_ms_token *token, char *pattern)
 		token = token -> next;
 	}
 	return (length);
-}
-
-size_t	ms_expand_wildcards(t_ms_token *token, char **paths)
-{
-	size_t		match_count;
-	int			pattern_index;
-	bool		is_absolute;
-	char *const	pattern = gc_add(ft_calloc(1, get_pattern(token, NULL) + 1));
-
-	get_pattern(token, pattern);
-	is_absolute = (pattern[0] == '/');
-	match_count = enumerate_matches(
-			pattern + is_absolute, gc_add(ft_strdup(&"/"[!is_absolute])), NULL);
-	if (paths == NULL)
-		return (gc_free_ptr(pattern), match_count + (match_count == 0));
-	match_count = enumerate_matches(
-			pattern + is_absolute,
-			gc_add(ft_strdup(&"/"[!is_absolute])), paths);
-	if (match_count != 0)
-		return (gc_free_ptr(pattern), match_count);
-	pattern_index = -1;
-	while (pattern[++pattern_index] != '\0')
-		if (pattern[pattern_index] == '\1')
-			pattern[pattern_index] = '*';
-	*paths = pattern;
-	return (1);
 }
